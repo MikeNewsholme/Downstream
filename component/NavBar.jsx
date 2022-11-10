0a05a0457844697ec576b6b0e3e9e041c991ac21
MikeNewsholme <100385842+MikeNewsholme@users.noreply.github.com>
2022-11-10 11:46:09 -0500

    finished styling the navbar

--- component/NavBar.jsx
+++ component/NavBar.jsx
@@ -14,11 +14,33 @@ const Navbar = () => {
           <div className={styles.text}>281-330-8004</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Home</li>
+          <li className={styles.listItem}>Software</li>
+          <li className={styles.listItem}>Hardware</li>
+          <div className={styles.listLogo}>
+            <Image
+              src="/img/logo.png"
+              alt="stream logo"
+              height="50"
+              width="50"
+            />
+          </div>
+
+          <li className={styles.listItem}>Accessories</li>
+          <li className={styles.listItem}>Gift Cards</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <Image src="/img/cart.png" alt="cart logo" height="50" width="50" />
+          <div className={styles.itemCounter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Navbar;
-//created a couple items for phone number/ website "shop"/ 
