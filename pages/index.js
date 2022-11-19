18f6670c1e48320aa721fe3a2f197b222d2969e5
MikeNewsholme <100385842+MikeNewsholme@users.noreply.github.com>
2022-11-19 10:32:47 -0500

    admin login completed and functional, stored session in cookie

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ import styles from "../styles/Home.module.css";
 import Slider from "../component/Slider";
 import Productlist from "../component/Products";
 import axios from "axios"
+import { useState } from 'react'
+import AddButton from "../component/AddButton";
 
-export default function Home({productList}) {
- 
+export default function Home({productList, admin}) {
+ const [close, setClose] = useState(true)
 
   return (
     <div className={styles.container}>
@@ -17,18 +19,29 @@ export default function Home({productList}) {
         <link rel="icon" href="/img/logo3.png" />
       </Head>
       <Slider/>
+      {admin && <AddButton setClose={setClose} />}
       <Productlist productList={productList}/>
+      {!close && <Add setClose={setClose}/>}
       
     </div>
   );
 }
 
  //In nextJS you can fetch all data before rendering components and then show components with those props
-export const getServerSideProps = async () =>{
+export const getServerSideProps = async (ctx) =>{
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true
+  }
+
+
   const res = await axios.get("http://localhost:3000/api/products");
   return{
     props:{
-      productList:res.data
+      productList:res.data,
+      admin
     }
   }
 }
